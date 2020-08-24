fcc288bd8c94d8995f3f33a9f44c7751dd45e280
JMinkyoung <37430920+JMinkyoung@users.noreply.github.com>
2020-08-24 14:10:52 +0900

    Update Tower of Hanoi.cpp

--- Tower of Hanoi.cpp
+++ Tower of Hanoi.cpp
@@ -4,16 +4,18 @@
 
 using namespace std;
 
-//하노이 탑 구현
-void hanoi(int n, int from, int to, int by) {
+void hanoi(int n, int from, int to) {
 
 	if (!n)
 		return;
 	else {
-		hanoi(n - 1, from, by, to);
+
+		//1번, 2번, 3번 기둥이고 세 기둥의 합이 6이므로
+		hanoi(n - 1, from, 6 - from - to);
 		printf("%d %d\n", from, to);
-		hanoi(n - 1, by, to, from);
+		hanoi(n - 1, 6 - from - to, to);
 	}
+
 }
 
 
@@ -23,15 +25,15 @@ int main() {
 	cin >> N;
 
 	int cnt = 1;
-	
-	//원판의 이동 횟수
+
 	for (int i = 0; i < N; i++) {
 		cnt *= 2;
 	}
 
 	cout << cnt - 1 << endl;
-	hanoi(N, 1, 3, 2);
-	
+
+	//처음엔 무조건 첫번째기둥->세번째기둥 
+	hanoi(N, 1, 3);
 	return 0;
-	
+
 }
