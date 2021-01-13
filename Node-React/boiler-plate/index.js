d057f9b7d185f688057afcc545b0a9b9d8b763b4
JMinkyoung <37430920+JMinkyoung@users.noreply.github.com>
2021-01-13 15:57:59 +0900

    Login까지 구현

--- Node-React/boiler-plate/index.js
+++ Node-React/boiler-plate/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const {User} = require("./models/User")
 const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 const config = require('./config/key')
 const app = express()
 const port = 5000
@@ -10,6 +11,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 //application/json
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 //moongoDB 연결
 const mongoose = require('mongoose')
@@ -27,6 +29,7 @@ app.post('/register',(req,res)=>{
     //회원가입 할 때 필요한 정보들을 clinet 에서 가져오면
     //그것들을 DB에 넣어준다.
     const user = new User(req.body);
+
     user.save((err,doc)=>{
         if(err) return res.json({success:false,err})
         return res.status(200).json({
@@ -35,4 +38,36 @@ app.post('/register',(req,res)=>{
     })
 
 })
+
+//로그인을 위한 route
+app.post('/login',(req,res)=>{
+
+    //요청된 이메일을 DB에 존재하는지 찾는다.
+    User.findOne({email:req.body.email},(err,user)=>{
+        if(!user){
+            return res.json({
+                loginSuccess: false,
+                message: "제공된 이메일에 해당하는 유저가 없습니다."
+            });
+        }
+    //요청된 이메일에 대한 비밀번호가 같은지 확인한다.
+        user.comparePassword(req.body.password,(err,isMatch)=>{
+            if(!isMatch)
+             return res.json({loginSuccess:false, message: "비밀번호가 틀렸습니다."});
+
+    //비밀번호까지 맞다면 token을 생성한다.
+        user.generateToken((err,user)=>{
+            if(err) return res.status(400).send(err);
+            
+            //토큰을 쿠키, 로컬등에 저장한다.
+            res.cookie("x_auth",user.token)
+            .status(200)
+            .json({loginSuccess:true, userId: user._id});
+
+        });
+
+        });
+    });
+});
+
 app.listen(port,() => console.log(`Example app listening on port ${port}!`))
