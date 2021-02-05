ea59b98f239084f1182794983953c56332610ff8
JMinkyoung <37430920+JMinkyoung@users.noreply.github.com>
2021-02-05 15:09:31 +0900

    6.4장

--- Node/Nodejs 교과서/learn-express/app.js
+++ Node/Nodejs 교과서/learn-express/app.js
@@ -6,6 +6,8 @@ const dotenv = require('dotenv');
 const path = require('path');
 
 dotenv.config();
+const indexRouter = require('./routes/index');
+const userRouter = require('./routes/user');
 const app = express();
 app.set('port', process.env.PORT || 3000);
 
@@ -27,18 +29,57 @@ app.use(session({
     name:'session-cookie',
 }));
 
-app.use((req,res,next)=>{
-    console.log('모든 요청에 다 실행됩니다');
-    next();
-});
+// const multer = require('multer');
+// const fs = require('fs');
 
-app.get('/',(req,res,next)=>{
-    console.log('GET / 요청에서만 실행됩니다.');
-    next();
-}, (req,res)=>{
-    throw new Error('에러는 에러 처리 미들웨어로 갑니다');
-});
+// try{
+//     fs.readdirSync('uploads');
+// }catch(error){
+//     console.log("uploads 폴더가 없어서 uploads 폴더를 생성합니다.");
+//     fs.mkdirSync("uploads");
+// }
 
+// const upload = multer({
+//     storage: multer.diskStorage({
+//         destination(req, file, done){
+//             done(null,'uploads/');
+//         },
+//         filename(req, file, done){
+//             const ext = path.extname(file.originalname);
+//             done(null, path.basename(file.originalname, ext) + Date.now() + ext);
+//         },
+//     }),
+//     limits: {fileSize: 5 * 1024 * 1024},
+// });
+
+// app.get('/upload', (req,res)=>{
+//     res.sendFile(path.join(__dirname, 'multipart.html'));
+// });
+// app.post('/upload',
+//     upload.fields([{name: 'image1'}, {name: 'image2'}]),
+//     (req,res)=>{
+//         console.log(req.files, req.body);
+//         res.send('ok');
+//     },
+// );
+
+// app.use((req,res,next)=>{
+//     console.log('모든 요청에 다 실행됩니다');
+//     next();
+// });
+
+// app.get('/',(req,res,next)=>{
+//     console.log('GET / 요청에서만 실행됩니다.');
+//     next();
+// }, (req,res)=>{
+//     throw new Error('에러는 에러 처리 미들웨어로 갑니다');
+// });
+
+app.use('/', indexRouter);
+app.use('/user', userRouter);
+// app.use((req,res,next)=>{
+//     res.status(404).send('Not Found');
+// });
 app.use((err,req,res,next)=>{
     console.error(err);
     res.status(500).send(err.message);
