e91675b6be85512cfb1abd3a2bb4bef7b262a950
JMinkyoung <37430920+JMinkyoung@users.noreply.github.com>
2021-02-18 17:52:03 +0900

    21/02/18

---
+++ Programmers/level2/가장 큰 정사각형 찾기.js
@@ -0,0 +1,21 @@
+function solution(board)
+{
+    let answer = 0; 
+    let row = board.length; 
+    let column = board[0].length;
+    
+    if(row<2 || column<2) return 1;
+    
+    for(let i=1; i<row; i++){
+        for(let j=1; j<column; j++){
+            if(board[i][j]===1){
+               board[i][j] = Math.min(board[i-1][j-1], board[i-1][j], board[i][j-1]) + 1;
+            }
+            if(answer<board[i][j]){
+                answer = board[i][j];
+            }
+        }
+    }
+
+    return answer*answer;
+}
