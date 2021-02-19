0053407a75b96ad6d083f961e46ad29dd02ca148
JMinkyoung <37430920+JMinkyoung@users.noreply.github.com>
2021-02-19 17:52:01 +0900

    Morden React Study

---
+++ React/begin-react/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Hello from './Hello';
+import Wrapper from './Wrapper';
+import Counter from './Counter';
+import InputSample from './InputSample';
+
+import "./App.css";
+
+function App() {
+  return (
+   <InputSample />
+  );
+}
+
+export default App;
